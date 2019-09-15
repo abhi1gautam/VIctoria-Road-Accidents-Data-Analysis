4ec4a1d9675bedde8564fdee91264630708004a0
Pris-S <pfs.plc@gmail.com>
2019-09-16 00:00:37 +1000

    prisscript

--- Scripts/pris_script.R
+++ Scripts/pris_script.R
@@ -6,16 +6,22 @@ glimpse(mergedFiles)
 ##### Reading files ----
 #Separate month and year
 accident <- accident %>% mutate(CLEANDATE = as.Date(accident$ACCIDENTDATE,"%d/%m/%y"))
-accident <- separate(accident,"ACCIDENTDATE",c("Day", "Month", "Year"))
+accident <- separate(accident,"ACCIDENTDATE",c("DAY", "MONTH", "YEAR"), remove = FALSE)
 glimpse(accident)
+
+#Combine all
+total_accident <- merge(person,accident,by="ACCIDENT_NO")
 
 #Missing Values
 library(Amelia)
 missmap(mergedFiles, main = "Missing values vs observed")
 
-#Crash by year
-mergedFiles %>%
-	group_by(Year)%>%
-	summarise
-	ggplot(aes(x=Year, y=))+
-	geom_col
+#Crash by year and severity
+total_accident %>%
+	group_by(YEAR)%>%
+	filter(YEAR > "2008")%>%
+	filter(YEAR < "2019")%>%
+	ggplot(aes(x=YEAR,))+
+	geom_bar()
+
+#crashes in wet weather
