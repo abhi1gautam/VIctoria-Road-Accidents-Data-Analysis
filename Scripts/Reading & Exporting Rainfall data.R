116d4c179fc37edd28716dad85bbdce35f6a0bbb
Bruno Quevedo <BrunoQuevedo@users.noreply.github.com>
2019-09-18 19:00:38 +1000

    scripts to read rainfall data and merge with accident data.

--- Scripts/Reading & Exporting Rainfall data.R
+++ Scripts/Reading & Exporting Rainfall data.R
@@ -1,39 +1,40 @@
+# This script reads the rainfall.nc data and reduce it to the shape of Victoria
+# Improvements should be done: Loop to read all files at once/read dynamic variables
+
+#### Libraries
 library(ncdf4)
 library(chron)
 library(RColorBrewer)
 library(lattice)
+library (tidyverse)
+library(sp)
+library(rgdal)
 
-# loop to read all rainfall datas from 2006 to 2019. 
-# Rename your daily_rainfall.nc file to year.nc (i.e 2006.nc 2007.nc ...)
+
+# Rename your daily_rainfall.nc file to year.nc (i.e 2017.nc 2018.nc ...)
 # Make sure to set working directory to read above files (Session > Set Working Directory ...)
-#for (i in 2017:2018) {
 	#Reading file
-	i<- 2017
+	i<- 2018 
 	daily_rain <- i
 	ncfname <- paste(daily_rain, ".nc", sep = "")
 	dname <- "daily_rain"
 	ncin <- nc_open(ncfname)
-	#	print(ncin)
-	
-	# Getting the longitude indexes which longitude values are between 140 and 150 (Victoria State)
-	lonIdx <- which( ncin$dim$lon$vals > 140 & ncin$dim$lon$vals < 150) ##
+
+	# Getting the longitude indexes which longitude values are between 140.9 and 149.8 (Victoria State)
+	lonIdx <- which( ncin$dim$lon$vals > 140.9 & ncin$dim$lon$vals < 149.8) ##
 	max(lonIdx)
 	# Reading longitude values. As the LonIdx varies from 1 (lon 140) to 199 (lon 150), we want 
 	#to read the 199 next values, starting at index 562 (which is lon 140)
 	lon <- ncvar_get(ncin, "lon", start=c(min(lonIdx)), count=c((max(lonIdx)-min(lonIdx)+1)))
 	nlon <- dim(lon)
-	#	head(lon)
+
+	# Getting the latitude indexes which latitude values are between -39.10 and -34 (Victoria State)
+	latIdx  <- which( ncin$dim$lat$vals > -39.10 & ncin$dim$lat$vals < -34)
 	
-	# Getting the latitude indexes which latitude values are between -40 and -33 (Victoria State)
-	latIdx  <- which( ncin$dim$lat$vals > -40 & ncin$dim$lat$vals < -33)
-	
-	# Reading latitude values. As the latIdx varies from 1 (lat -40) to 139 (lat -33), we want 
-	#to read the 139 next values, starting at index 82 (which is lat -40)
+	# Reading latitude values.
 	lat <- ncvar_get(ncin, "lat", verbose = F, start=c(min(latIdx)), count=c((max(latIdx)-min(latIdx)+1)))
 	nlat <- dim(lat)
-	#	head(lat)
-	#	print(c(nlon, nlat))
-	
+
 	# Reading time values (dates)
 	t <- ncvar_get(ncin, "time")
 	tunits <- ncatt_get(ncin, "time", "units")
@@ -44,8 +45,7 @@ library(lattice)
 	dlname <- ncatt_get(ncin, dname, "long_name")
 	dunits <- ncatt_get(ncin, dname, "units")
 	fillvalue <- ncatt_get(ncin, dname, "_FillValue")
-	#	dim(daily_rain.array)
-	
+
 	# Getting global attributes
 	#	title <- ncatt_get(ncin, 0, "title")
 	#	institution <- ncatt_get(ncin, 0, "institution")
@@ -64,64 +64,68 @@ library(lattice)
 	
 	# Replace NetCDF fillvalues with R NAs
 	daily_rain.array[daily_rain.array == fillvalue$value] <- NA
-	#	length(na.omit(as.vector(daily_rain.array[, , 1])))
-	
+
 	# Getting a single time slice of the data (only one day), create an R data frame, and write a .csv file
-	#m <- 1
-	#daily_rain.slice <- daily_rain.array[, , m]
+  m <- 1
+	daily_rain.slice2 <- daily_rain.df2018[, , m]
 	
 	# Visualization
-	#grid <- expand.grid(lon = lon, lat = lat)
-	#cutpts <- c(-50, -40, -30, -20, -10, 0, 10, 20, 30, 40, 50)
-	#levelplot(daily_rain.slice ~ lon * lat, data = grid, at = cutpts, cuts = 11, pretty = T, 
-	#					col.regions = (rev(brewer.pal(10, "RdBu"))))
+	grid <- expand.grid(lon = lon, lat = lat)
+	cutpts <- c(-50, -40, -30, -20, -10, 0, 10, 20, 30, 40, 50)
+	levelplot(daily_rain.slice ~ lon * lat, data = grid, at = cutpts, cuts = 11, pretty = T, 
+						col.regions = (rev(brewer.pal(10, "RdBu"))))
 	
 	# Expand.grid() function is used to create pairs of values of longtiude and latitude
 	# As.vector() function is used to “unstack” the slice of data into a long vector,
-	#lonlat <- expand.grid(lon, lat)
-	#daily_rain.vec <- as.vector(daily_rain.slice)
-	#length(daily_rain.vec)
+ #	lonlat <- expand.grid(lon, lat)
+ #	daily_rain.vec <- as.vector(daily_rain.slice)
+ #	length(daily_rain.vec)
 	
 	
 	# The data.frame() and cbind() functions are used to assemble the columns of the data frame, which are assigned appropriate names using the names() function (on the left-hand side of assignment operator). The head() function, applied on top of the na.omit() function lists the first rows of values without NAs:
 	#daily_rain.df01 <- data.frame(cbind(lonlat, daily_rain.vec))
 	#names(daily_rain.df01) <- c("lon", "lat", paste(dname, as.character(m), sep = "_"))
 	#head(na.omit(daily_rain.df01), 20)
-	
-	# Exporting to CSV
-	#csvfile <- "cru_daily_rain_1.csv"
-	#write.table(na.omit(daily_rain.df01), csvfile, row.names = FALSE, sep = ",")
-	
+
 	# Converting the whole array to a data frame
 	#Convert the nlon by nlat by nt array into a nlon by nlat by nt matrix
 	daily_rain.vec.long <- as.vector(daily_rain.array)
-	#	length(daily_rain.vec.long)
 	
 	# Reshape vector into  a matrix using the matrix() function
 	daily_rain.mat <- matrix(daily_rain.vec.long, nrow = nlon * nlat, ncol = nt)
-	dim(daily_rain.mat)
-	#	head(na.omit(daily_rain.mat))
 	
 	# Create the second data frame from the daily_rain.mat matrix.
 	lonlat <- expand.grid(lon, lat)
 	nam <- paste("daily_rain.df", i, sep = "")
 	assign(nam, data.frame(cbind(lonlat, daily_rain.mat)))
-	#	daily_rain.df02 <- data.frame(cbind(lonlat, daily_rain.mat))
 	options(width = 110)
-	#	head(na.omit(daily_rain.df02, 20))
-	
-
-  names(daily_rain.df2017)[3:(max(t)+3)] <- format(seq(as.Date("2017/01/01"), by = "day", length.out = (max(t)+1)))
-
-  daily_rain.df2017 <- as.data.frame(daily_rain.df2017) %>%
-	tidyr::gather(key = DATE, value = Rainfall, -Var1, -Var2)
   
-  daily_rain.df2017 <- daily_rain.df2017 %>% rename (Lat = Var2)
-  daily_rain.df2017 <- daily_rain.df2017 %>% rename (Long = Var1)
-
-  daily_rain.df2017$DATE  <- as.Date(daily_rain.df2017$DATE, "%Y-%m-%d")
+	#Rename Var1 and Var2 columns to Long and Lat
+  daily_rain.df2018 <- daily_rain.df2018 %>% rename (Lat = Var2)
+  daily_rain.df2018 <- daily_rain.df2018 %>% rename (Long = Var1)
   
+  #Reading Victoria shapefile
+  wa.map <- readOGR(dsn = "Standard", layer = "VIC_STATE_POLYGON_shp")
+  #Transforming the dataframe into a SpatialPointsDataFrame 
+  coordinates(daily_rain.df2018) <- ~ Long + Lat
+  proj4string(daily_rain.df2018) <- proj4string(wa.map)
+  
+  # Shaping the SpatialPointsDataFrame to Victoria shape
+    daily_rain.df2018 <- daily_rain.df2018[!is.na(over(daily_rain.df2018, as(wa.map, "SpatialPolygons"))), ]
+#  plot(daily_rain.df2018)
+  
+   # Transforming the SpatialPointsDataFrame to Data Frame
+   daily_rain.df2018 <- as.data.frame(daily_rain.df2018)
+   # Renaming the columns to Date format
+   names(daily_rain.df2018)[3:(max(t)+3)] <- format(seq(as.Date("2018/01/01"), by = "day", length.out = (max(t)+1)))
+   
+   #Tidying data, transforming date columns into a single column
+  daily_rain.df2018 <- as.data.frame(daily_rain.df2018) %>%
+	tidyr::gather(key = DATE, value = Rainfall, -Long, -Lat)
+  
+  # Formating Date field as Date
+  daily_rain.df2018$DATE  <- as.Date(daily_rain.df2018$DATE, "%Y-%m-%d")
   
   # Export to CSV
 # csvfile2 <- "cru_daily_rain_2.csv"
-# write.table(na.omit(daily_rain.df2017), csvfile2, row.names = FALSE, sep = ",")
+# write.table(na.omit(daily_rain.df2018), csvfile2, row.names = FALSE, sep = ",")
--- Scripts/script.R
+++ Scripts/script.R
@@ -3,6 +3,8 @@ library(tidyverse)
 library(esquisse)
 library(geosphere)
 library(tibble)
+library(sqldf)
+library(plyr)
 ##### Reading files ----
 accident <- read.csv("Datasets/Road Crashes/ACCIDENT.csv")
 accident_node <-  read.csv("Datasets/Road Crashes/NODE.CSV")
@@ -12,18 +14,29 @@ accident_node <-  read.csv("Datasets/Road Crashes/NODE.CSV")
 #Converting fct to date
 accident$ACCIDENTDATE <- as.Date(accident$ACCIDENTDATE, "%d/%m/%Y")
 
-#Renaming date columns (to be used as primary key btw accidents and climate data)
+#Renaming date columns (to be used as primary key btw accidents files)
 accident <- accident %>% rename (DATE = ACCIDENTDATE)
 
 #### Merging Data ----
-#Creating mergedFiles object, which contains accident, accident location and climate data
-mergedFiles <- merge(accident,accident_node,by="ACCIDENT_NO")
+#Creating mergedFiles object, which contains accident and accident location
+mergedFiles <- merge(accident,accident_node[!duplicated(accident_node$ACCIDENT_NO), ],by="ACCIDENT_NO")
 
+### Rounding latitude and longitude to 2 decimal places in order to merge with rainfall dataset
 mergedFiles$Lat <-  round(mergedFiles$Lat, 2)
 mergedFiles$Lat <-  0.05*(round(mergedFiles$Lat/0.05))
-
 mergedFiles$Long <-  round(mergedFiles$Long, 2)
 mergedFiles$Long <-  0.05*(round(mergedFiles$Long/0.05))
 
+#Merging the Accident tables with the rainfall data
+mergedFiles <- merge(mergedFiles, daily_rain.df2019, by=c("Lat","Long","DATE"),all.x = TRUE)
+mergedFiles <- mergedFiles %>% rename (Rainfall2019 = Rainfall)
 
-mergedFiles <- merge(mergedFiles, daily_rain.df2017, by=c("Lat","Long","DATE"),all.x = TRUE)
+# Combining all rainfall data in one column
+mergedFiles$Rainfall <- coalesce(mergedFiles$Rainfall2006,mergedFiles$Rainfall2007,mergedFiles$Rainfall2008, mergedFiles$Rainfall2009, mergedFiles$Rainfall2010, mergedFiles$Rainfall2011, mergedFiles$Rainfall2012, mergedFiles$Rainfall2013, mergedFiles$Rainfall2014, mergedFiles$Rainfall2015, mergedFiles$Rainfall2016, mergedFiles$Rainfall2017, mergedFiles$Rainfall2018, mergedFiles$Rainfall2019)
+
+# Export to CSV
+acccsvfile <- "Accident_with_Rainfall"
+write.table(mergedFiles, acccsvfile, row.names = FALSE, sep = ",")
+
+
+ 
